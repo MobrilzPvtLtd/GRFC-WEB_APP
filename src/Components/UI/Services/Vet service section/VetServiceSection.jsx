8ec8113c19a4e49aecf6a8e09ccc640fa10f77e2
Sanjay Kushwah <sanjay@mobrilz.agency>
2024-11-22 17:42:03 +0530

    category_api_Subcate_Api_done

--- src/Components/UI/Services/Vet service section/VetServiceSection.jsx
+++ src/Components/UI/Services/Vet service section/VetServiceSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import video_bg from "../../../../images_new/services-video.jpg"
 import primare_care from "../../../../images_new/lila-in-primary-care.gif"
 import doctor from "../../../../images_new/doctor2000011111.png"
@@ -10,33 +10,30 @@ import ServiceBreadcrum from '../Service Breadcrum/ServiceBreadcrum';
 import bg from "../../../../images_new/background_2.png"
 import banner_1 from "../../../../images_new/banner-img-1-1.jpg"
 import banner_2 from "../../../../images_new/banner-img-2.jpg"
+import axios from 'axios';
+
 
 const VetServicesSection = () => {
+  
+  let url = process.env.REACT_APP_BACKEND_BASE_URL;
+  const[subcategory_data, setSubCategory_data]= useState()
+  useEffect(() => {
+    const fetchSubCategoryData = async () => {
+      try {
+        const res = await axios.get(`${url}/subcategory`);
+        setSubCategory_data(res.data.data)
+        // console.log(res)
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchSubCategoryData();
+  }, []);
   return (
-//     <section class="gap services">
-//     <div class="container">
-//         <div class="row">
-            
-//             <div class="col-lg-12">
-//                 <div class="video position-relative">
-//                         <a data-fancybox="" href="https://www.youtube.com/watch?v=xKxrkht7CpY">
-//                             <i>
-//                               <svg width="11" height="17" viewBox="0 0 11 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                               <path d="M11 8.49951L0.5 0.27227L0.5 16.7268L11 8.49951Z" fill="#000"></path>
-//                               </svg>
-//                             </i>
-//                         </a>
-//                         <figure>
-//                             <img src={video_bg} alt="img"/>
-//                         </figure>
-//                     </div>
-//             </div>
-            
-//         </div>
-//     </div>
-// </section>
+
 <> 
-{/* <ServiceBreadcrum/> */}
+
 
 <section
     className="banner"
@@ -86,6 +83,8 @@ const VetServicesSection = () => {
 <section className="gap no-bottom">
       <div className="container mt-6">
         <div className="row">
+          {  
+               subcategory_data?.map((item,index)=>( 
           <div className="col-lg-4 col-md-6">
             <div className="we-provide">
               <div className="we-provide-img a01 flex justify-center">
@@ -99,13 +98,13 @@ const VetServicesSection = () => {
                   ></path>
                 </svg>
               </div>
-              <a href="/services/farmica/urgencies">
-                <h5>Urgencias</h5>
+              <a href="/services/farmica/prescription">
+                <h5>{item?.name}</h5>
               </a>
             </div>
           </div>
-
-          <div className="col-lg-4 col-md-6">
+          )) }
+          {/* <div className="col-lg-4 col-md-6">
             <div className="we-provide">
               <div className="we-provide-img a01 flex justify-center">
                 <img src={doctor} alt="we-provide-2" />
@@ -196,6 +195,42 @@ const VetServicesSection = () => {
               </a>
             </div>
           </div>
+          <div className="col-lg-4 col-md-6">
+            <div className="we-provide mb-0">
+              <div className="we-provide-img a01 flex justify-center">
+                <img src={vet} alt="we-provide-3" />
+                <svg width="326" height="326" viewBox="0 0 673 673" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M9.82698 416.603C-19.0352 298.701 18.5108 173.372 107.497 90.7633L110.607 96.5197C24.3117 177.199 -12.311 298.935 15.0502 413.781L9.82698 416.603ZM89.893 565.433C172.674 654.828 298.511 692.463 416.766 663.224L414.077 658.245C298.613 686.363 175.954 649.666 94.9055 562.725L89.893 565.433ZM656.842 259.141C685.039 374.21 648.825 496.492 562.625 577.656L565.413 582.817C654.501 499.935 691.9 374.187 662.536 256.065L656.842 259.141ZM581.945 107.518C499.236 18.8371 373.997 -18.4724 256.228 10.5134L259.436 16.4515C373.888 -10.991 495.248 25.1518 576.04 110.708L581.945 107.518Z"
+                    fill="#fedc4f"
+                  ></path>
+                </svg>
+              </div>
+              <a href="/services/icu/Cremacion">
+                <h5>Cremacion</h5>
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <div className="we-provide mb-0">
+              <div className="we-provide-img a01 flex justify-center">
+                <img src={vet} alt="we-provide-3" />
+                <svg width="326" height="326" viewBox="0 0 673 673" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M9.82698 416.603C-19.0352 298.701 18.5108 173.372 107.497 90.7633L110.607 96.5197C24.3117 177.199 -12.311 298.935 15.0502 413.781L9.82698 416.603ZM89.893 565.433C172.674 654.828 298.511 692.463 416.766 663.224L414.077 658.245C298.613 686.363 175.954 649.666 94.9055 562.725L89.893 565.433ZM656.842 259.141C685.039 374.21 648.825 496.492 562.625 577.656L565.413 582.817C654.501 499.935 691.9 374.187 662.536 256.065L656.842 259.141ZM581.945 107.518C499.236 18.8371 373.997 -18.4724 256.228 10.5134L259.436 16.4515C373.888 -10.991 495.248 25.1518 576.04 110.708L581.945 107.518Z"
+                    fill="#fedc4f"
+                  ></path>
+                </svg>
+              </div>
+              <a href="/services/icu/OrientacionTelefonica">
+                <h5>Orientacion Telefonica</h5>
+              </a>
+            </div>
+          </div> */}
         </div>
       </div>
     </section>
--- src/Components/UI/Services/service section/ServicesSection.jsx
+++ src/Components/UI/Services/service section/ServicesSection.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import primare_care from "../../../../images_new/lila-in-primary-care.gif"
 import doctor from "../../../../images_new/doctor2000011111.png"
 import cardiology from "../../../../images_new/cardiology.png"
@@ -11,13 +11,15 @@ import axios from 'axios';
 const ServicesSection = () => {
 
   let url = process.env.REACT_APP_BACKEND_BASE_URL;
+  const[category_data,setCategory_data] = useState([]);
+
 
   useEffect(() => {
     const fetchCategoryData = async () => {
       try {
         const res = await axios.get(`${url}/category`);
-
-        console.log(res)
+        setCategory_data(res.data.data)
+        // console.log(res)
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
@@ -25,14 +27,19 @@ const ServicesSection = () => {
 
     fetchCategoryData();
   }, []);
+  console.log('1111111111',category_data)
   return (
     <section className="gap no-bottom">
       <div className="container">
         <div className="row">
-          <div className="col-lg-4 col-md-6">
+          { category_data?.map((item,index)=>(
+              
+            
+            <div  className="col-lg-4 col-md-6" key={index}>
             <div className="we-provide">
-              <div className="we-provide-img a01 flex justify-center">
-                <img src={primare_care} alt="we-provide-1" />
+              <div className="we-provide-img a01 flex justify-center my-5">
+                
+                <img className="w-60 aspect-square" src={item?.image_link} alt="we-provide-1" />
                 <svg width="326" height="326" viewBox="0 0 673 673" xmlns="http://www.w3.org/2000/svg">
                   <path
                     fillRule="evenodd"
@@ -43,102 +50,12 @@ const ServicesSection = () => {
                 </svg>
               </div>
               <a href="/services/farmacia">
-                <h5>Farmacia</h5>
+                <h5><b>{item?.name} </b></h5>
               </a>
             </div>
-          </div>
-
-          <div className="col-lg-4 col-md-6">
-            <div className="we-provide">
-              <div className="we-provide-img a01 flex justify-center">
-                <img src={doctor} alt="we-provide-2" />
-                <svg width="326" height="326" viewBox="0 0 673 673" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M9.82698 416.603C-19.0352 298.701 18.5108 173.372 107.497 90.7633L110.607 96.5197C24.3117 177.199 -12.311 298.935 15.0502 413.781L9.82698 416.603ZM89.893 565.433C172.674 654.828 298.511 692.463 416.766 663.224L414.077 658.245C298.613 686.363 175.954 649.666 94.9055 562.725L89.893 565.433ZM656.842 259.141C685.039 374.21 648.825 496.492 562.625 577.656L565.413 582.817C654.501 499.935 691.9 374.187 662.536 256.065L656.842 259.141ZM581.945 107.518C499.236 18.8371 373.997 -18.4724 256.228 10.5134L259.436 16.4515C373.888 -10.991 495.248 25.1518 576.04 110.708L581.945 107.518Z"
-                    fill="#fb5e3c"
-                  ></path>
-                </svg>
-              </div>
-              <a href="/services/VetUCI">
-                <h5>Vet UCI</h5>
-              </a>
-            </div>
-          </div>
-
-          <div className="col-lg-4 col-md-6">
-            <div className="we-provide mb-0">
-              <div className="we-provide-img a01 flex justify-center">
-                <img src={cardiology} alt="we-provide-3" />
-                <svg width="326" height="326" viewBox="0 0 673 673" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M9.82698 416.603C-19.0352 298.701 18.5108 173.372 107.497 90.7633L110.607 96.5197C24.3117 177.199 -12.311 298.935 15.0502 413.781L9.82698 416.603ZM89.893 565.433C172.674 654.828 298.511 692.463 416.766 663.224L414.077 658.245C298.613 686.363 175.954 649.666 94.9055 562.725L89.893 565.433ZM656.842 259.141C685.039 374.21 648.825 496.492 562.625 577.656L565.413 582.817C654.501 499.935 691.9 374.187 662.536 256.065L656.842 259.141ZM581.945 107.518C499.236 18.8371 373.997 -18.4724 256.228 10.5134L259.436 16.4515C373.888 -10.991 495.248 25.1518 576.04 110.708L581.945 107.518Z"
-                    fill="#fedc4f"
-                  ></path>
-                </svg>
-              </div>
-              <a href="/services/Medi+cotas">
-                <h5>Medi+cotas</h5>
-              </a>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-6">
-            <div className="we-provide mb-0">
-              <div className="we-provide-img a01 flex justify-center">
-                <img src={feline} alt="we-provide-3" />
-                <svg width="326" height="326" viewBox="0 0 673 673" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M9.82698 416.603C-19.0352 298.701 18.5108 173.372 107.497 90.7633L110.607 96.5197C24.3117 177.199 -12.311 298.935 15.0502 413.781L9.82698 416.603ZM89.893 565.433C172.674 654.828 298.511 692.463 416.766 663.224L414.077 658.245C298.613 686.363 175.954 649.666 94.9055 562.725L89.893 565.433ZM656.842 259.141C685.039 374.21 648.825 496.492 562.625 577.656L565.413 582.817C654.501 499.935 691.9 374.187 662.536 256.065L656.842 259.141ZM581.945 107.518C499.236 18.8371 373.997 -18.4724 256.228 10.5134L259.436 16.4515C373.888 -10.991 495.248 25.1518 576.04 110.708L581.945 107.518Z"
-                    fill="#fedc4f"
-                  ></path>
-                </svg>
-              </div>
-              <a href="/services/VetCardiología">
-                <h5>Vet Cardiología</h5>
-              </a>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-6">
-            <div className="we-provide mb-0">
-              <div className="we-provide-img a01 flex justify-center">
-                <img src={vet} alt="we-provide-3" />
-                <svg width="326" height="326" viewBox="0 0 673 673" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M9.82698 416.603C-19.0352 298.701 18.5108 173.372 107.497 90.7633L110.607 96.5197C24.3117 177.199 -12.311 298.935 15.0502 413.781L9.82698 416.603ZM89.893 565.433C172.674 654.828 298.511 692.463 416.766 663.224L414.077 658.245C298.613 686.363 175.954 649.666 94.9055 562.725L89.893 565.433ZM656.842 259.141C685.039 374.21 648.825 496.492 562.625 577.656L565.413 582.817C654.501 499.935 691.9 374.187 662.536 256.065L656.842 259.141ZM581.945 107.518C499.236 18.8371 373.997 -18.4724 256.228 10.5134L259.436 16.4515C373.888 -10.991 495.248 25.1518 576.04 110.708L581.945 107.518Z"
-                    fill="#fedc4f"
-                  ></path>
-                </svg>
-              </div>
-              <a href="/services/VetCentroFelino">
-                <h5>Vet Centro Felino</h5>
-              </a>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-6">
-            <div className="we-provide mb-0">
-              <div className="we-provide-img a01 flex justify-center">
-                <img src={dog} alt="we-provide-3" />
-                <svg width="326" height="326" viewBox="0 0 673 673" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M9.82698 416.603C-19.0352 298.701 18.5108 173.372 107.497 90.7633L110.607 96.5197C24.3117 177.199 -12.311 298.935 15.0502 413.781L9.82698 416.603ZM89.893 565.433C172.674 654.828 298.511 692.463 416.766 663.224L414.077 658.245C298.613 686.363 175.954 649.666 94.9055 562.725L89.893 565.433ZM656.842 259.141C685.039 374.21 648.825 496.492 562.625 577.656L565.413 582.817C654.501 499.935 691.9 374.187 662.536 256.065L656.842 259.141ZM581.945 107.518C499.236 18.8371 373.997 -18.4724 256.228 10.5134L259.436 16.4515C373.888 -10.991 495.248 25.1518 576.04 110.708L581.945 107.518Z"
-                    fill="#fedc4f"
-                  ></path>
-                </svg>
-              </div>
-              <a href="/services/VetHotel">
-                <h5>Vet Hotel</h5>
-              </a>
-            </div>
-          </div>
+           </div>  
+            ))}
+          
         </div>
       </div>
     </section>
