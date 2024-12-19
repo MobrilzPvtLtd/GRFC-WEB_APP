c9b6e0fb2c344a83a9914721b392343e37f1cacf
Sanjay-mobrilz <sanjay@mobrilz.agency>
2024-12-19 16:07:31 +0530

    testing_5

--- src/Components/UI/OrderConfirmation/OrderConfirmation.jsx
+++ src/Components/UI/OrderConfirmation/OrderConfirmation.jsx
@@ -3,17 +3,16 @@ import { ValueContext } from "../../Context/Context_Hook";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-
 const OrderConfirmation = () => {
   const context = useContext(ValueContext);
-   const [TransactionId, setTransactionId] = useState(null);
-    const [transactionStatus, setTransactionStatus] = useState("pending"); // Success or Failed
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const url = process.env.REACT_APP_BACKEND_BASE_URL;
-    const navigate = useNavigate()
+  const [TransactionId, setTransactionId] = useState(null);
+  const [transactionStatus, setTransactionStatus] = useState("pending"); // Success or Failed
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const url = process.env.REACT_APP_BACKEND_BASE_URL;
+  const navigate = useNavigate();
 
-    const localpaytmtoken = localStorage.getItem('transactiontoken')
-    
+  const localpaytmtoken = localStorage.getItem("transactiontoken");
+
   useEffect(() => {
     const fetchTransactionData = async () => {
       try {
@@ -26,75 +25,87 @@ const OrderConfirmation = () => {
         if (TransactionId?.status === "PAID") {
           setTransactionStatus("success");
           // navigate('/')
-
-        } else if(TransactionId?.status === "FAILED") {
+        } else if (TransactionId?.status === "FAILED") {
           setTransactionStatus("failed");
           //  navigate('/about-us')
         }
       } catch (error) {
         console.error("Error fetching user data:", error);
-        setTransactionStatus("failed"); 
+        setTransactionStatus("failed");
         setIsModalOpen(true);
       }
     };
-    if(localpaytmtoken) {
+    if (localpaytmtoken) {
       fetchTransactionData();
     }
   }, []);
 
-  // const urlParams = new URLSearchParams(window.location.search);
-  // const localtokenpaytm = urlParams.get('token');
-
   const closeModal = () => {
     setIsModalOpen(false);
-    setTransactionStatus(null); 
+    setTransactionStatus(null);
   };
 
-  console.log('transaction daata',TransactionId , 'paytm token',context.paymentToken , 'vvvv', transactionStatus, 'local',localpaytmtoken)
+  console.log(
+    "transaction daata",
+    TransactionId,
+    "paytm token",
+    context.paymentToken,
+    "vvvv",
+    transactionStatus,
+    "local",
+    localpaytmtoken,
+    "trans000",
+    TransactionId[0].id
+  );
 
   return (
     <>
       <div className="mt-64">
-       
-
         {transactionStatus === "pending" && (
-                  <div className="d-flex justify-content-center align-items-center vh-100">
-                  <div className="card shadow-lg p-4 text-center" style={{ maxWidth: '400px' }}>
-                    <div className="card-body">
-                      <h2 className="text-warning">Transaction Pending</h2>
-                      <h6>Your Transaction ID:{TransactionId?.id}</h6>
-                      <p className="mt-3">Your transaction has been Pending.</p>
-                    </div>
-                  </div>
-                </div>
-                
-                )}
-                {transactionStatus === "success" && (
-                 <div className="d-flex justify-content-center align-items-center vh-100">
-                 <div className="card shadow-lg p-4 text-center" style={{ maxWidth: '400px' }}>
-                   <div className="card-body">
-                     <h2 className="text-success">Transaction Successful</h2>
-                     <h6>Your Transaction ID:{TransactionId?.id}</h6>
-                     <p className="mt-3">Your transaction has been completed successfully.</p>
-                   </div>
-                 </div>
-               </div>
-               
-                )}
-                {transactionStatus === "failed" && (
-                 <div className="d-flex justify-content-center align-items-center vh-100">
-                 <div className="card shadow-lg p-4 text-center" style={{ maxWidth: '400px' }}>
-                   <div className="card-body">
-                     <h2 className="text-danger">Transaction Failed</h2>
-                     <h6>Your Transaction ID:{TransactionId?.id}</h6>
-                     <p className="mt-3">Your transaction has been Failed.</p>
-                   </div>
-                 </div>
-               </div>
-               
-                )}
+          <div className="d-flex justify-content-center align-items-center vh-100">
+            <div
+              className="card shadow-lg p-4 text-center"
+              style={{ maxWidth: "400px" }}
+            >
+              <div className="card-body">
+                <h2 className="text-warning">Transaction Pending</h2>
+                <h6>Your Transaction ID:{TransactionId[0]?.id}</h6>
+                <p className="mt-3">Your transaction has been Pending.</p>
+              </div>
+            </div>
+          </div>
+        )}
+        {transactionStatus === "success" && (
+          <div className="d-flex justify-content-center align-items-center vh-100">
+            <div
+              className="card shadow-lg p-4 text-center"
+              style={{ maxWidth: "400px" }}
+            >
+              <div className="card-body">
+                <h2 className="text-success">Transaction Successful</h2>
+                <h6>Your Transaction ID:{TransactionId[0]?.id}</h6>
+                <p className="mt-3">
+                  Your transaction has been completed successfully.
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+        {transactionStatus === "failed" && (
+          <div className="d-flex justify-content-center align-items-center vh-100">
+            <div
+              className="card shadow-lg p-4 text-center"
+              style={{ maxWidth: "400px" }}
+            >
+              <div className="card-body">
+                <h2 className="text-danger">Transaction Failed</h2>
+                <h6>Your Transaction ID:{TransactionId[0]?.id}</h6>
+                <p className="mt-3">Your transaction has been Failed.</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-      
     </>
   );
 };
